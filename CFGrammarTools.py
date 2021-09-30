40f82273baa0225fa3ead88656163ed72f1477f6
niklas <niklasgutting@gmx.de>
2021-09-30 09:59:56 +0200

    Remove chain productions

--- CFGrammarTools.py
+++ CFGrammarTools.py
@@ -1,5 +1,7 @@
+from functools import reduce
+
 class CFGrammar():
-    __symbolID = 1
+    __symbolID = 0
 
     def __init__(self, n, t, p, s):
         self._nonterminals = n
@@ -23,10 +25,12 @@ class CFGrammar():
     def start(self):
         return self._start
 
+    def getRules(self, n):
+        return self._productions[n].copy()
+
     def newSymbolName(name):
-        name = "%s#%i" % (name, CFGrammar.__symbolID)
         CFGrammar.__symbolID += 1
-        return name
+        return "%s#%i" % (name, CFGrammar.__symbolID)
 
     def nullables(self):
         nullables = set()
@@ -35,13 +39,27 @@ class CFGrammar():
                 nullables.add(lhs)
 
         oldNullables = set()
-        while (nullables != oldNullables):
+        while nullables != oldNullables:
             oldNullables = nullables
             for lhs, rhss in self.productions.items():
                 if any(set(rhs).issubset(nullables) for rhs in rhss):
                     nullables.add(lhs)
 
         return nullables
+
+    def chainProductions(self):
+        chainProductions = {} 
+        for n in self.nonterminals:
+            chainProductions[n] = {n}.union({rhs[0] for rhs in self.productions[n] 
+                                            if len(rhs) == 1 and rhs[0] in self.nonterminals})
+
+        oldChainProductions = {}
+        while chainProductions != oldChainProductions:
+            oldChainProductions = chainProductions
+            for n, cs in chainProductions.items():
+                chainProductions[n] = reduce(lambda a, b: a.union(chainProductions[b]), cs, set())
+
+        return chainProductions
 
 
 def nullDerivables(nullables, alpha):
@@ -73,15 +91,29 @@ def removeEpsilonProductions(G):
     p[name] = {(G.start,), ()} if G.start in nullables else {(G.start,)}
     return CFGrammar(n, G.terminals, p, name)
 
-# G = CFGrammar(
-#         {"A", "B"}, 
-#         {"a", "b"}, 
-#         {
-#             "A" : {("a",), ("B", "B")},
-#             "B" : {("b", "A"), ()},
-#         },
-#         "A"
-# )
+def removeChainProductions(G):
+    p = {}
+    nonterminals = G.nonterminals
+    chainProductions = G.chainProductions()
+
+    for n in nonterminals:
+        rules = reduce(lambda a, b: a.union(G.getRules(b)), chainProductions[n], set())
+        p[n] = {r for r in rules if not (len(r) == 1 and r[0] in nonterminals)} 
+
+    return CFGrammar(nonterminals, G.terminals, p, G.start)
+
+
+G = CFGrammar(
+        {"A", "B", "C"}, 
+        {"a", "b"}, 
+        {
+            "A" : {("a",), ("B",)},
+            "B" : {("b", "A"), ()},
+            "C" : {("A",)}
+        },
+        "A"
+)
 # G_ = removeEpsilonProductions(G)
-# print(G_.productions)
+print(G.productions)
+print(removeChainProductions(G).productions)
 
