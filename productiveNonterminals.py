ed106bc13cc74737fffcb0f4364d51f17b4902e1
niklas <niklasgutting@gmx.de>
2021-10-01 08:16:48 +0200

    Remove nonproductive nonterminals

--- productiveNonterminals.py
+++ productiveNonterminals.py
@@ -14,3 +14,14 @@ def computeProductiveNonterminals(G):
                 productive.add(n)
 
     return productive
+
+def removeNonproductiveNonterminals(G):
+    productive = computeProductiveNonterminals(G)
+    p = {}
+    for n in G.nonterminals:
+        if n in productive:
+            p[n] = frozenset([rhs for rhs in G.productions[n] 
+                                  if all(a in (G.terminals | productive) for a in rhs)])
+
+    return CFGrammar(productive, G.terminals, p, G.start)
+
